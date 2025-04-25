9532d488cd093893269ebc2749b1fd4660ac0a06
Rijarilala <rijarilalarasolofomanana@gmail.com>
2025-04-25 10:24:45 +0300

    essai

--- src/components/home/ImmigrationOverview.tsx
+++ src/components/home/ImmigrationOverview.tsx
@@ -37,11 +37,11 @@ const ImmigrationOverview = () => {
                 </li>)}
             </ul>
 
-            <Button size="lg" onClick={handleEligibilityClick} className="bg-migrapro-terre-cuite hover:bg-migrapro-terre-cuite/90 mx-[68px]">
+         
+          </div>
+          <Button size="lg" onClick={handleEligibilityClick} className="bg-migrapro-terre-cuite hover:bg-migrapro-terre-cuite/90 mx-[68px]">
               Tester mon éligibilité
             </Button>
-          </div>
-
           <div className="md:w-1/2">
             <img src="https://images.unsplash.com/photo-1508693926297-1d61ee3df82a?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" alt="Immigration Canada" className="rounded-lg shadow-xl w-full object-cover h-auto" />
           </div>
