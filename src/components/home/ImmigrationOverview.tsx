1beb74dc674dd02c639c1dbd2d878cbed512b77c
Rijarilala <rijarilalarasolofomanana@gmail.com>
2025-04-25 10:30:17 +0300

    okok

--- src/components/home/ImmigrationOverview.tsx
+++ src/components/home/ImmigrationOverview.tsx
@@ -37,7 +37,7 @@ const ImmigrationOverview = () => {
                 </li>)}
             </ul>
 
-            <Button size="lg" onClick={handleEligibilityClick} className="bg-migrapro-terre-cuite hover:bg-migrapro-terre-cuite/90 font-light text-base text-center text-slate-50 rounded-3xl py-[18px] my-[29px] mx-[238px] px-[126px]">
+            <Button size="lg" onClick={handleEligibilityClick} className="bg-migrapro-terre-cuite hover:bg-migrapro-terre-cuite/90 mx-[68px]">
               Tester mon éligibilité
             </Button>
           </div>
@@ -46,16 +46,6 @@ const ImmigrationOverview = () => {
             <img src="https://images.unsplash.com/photo-1508693926297-1d61ee3df82a?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" alt="Immigration Canada" className="rounded-lg shadow-xl w-full object-cover h-auto" />
           </div>
         </div>
-        <p className="text-lg text-gray-600 mb-8">
-        🎯 Vous souhaitez mettre toutes les chances de votre côté ?
-
-Trouver un emploi au Canada demande plus que de bons documents — il faut une stratégie complète, adaptée et percutante.
-
-C’est pourquoi nous avons créé le Pack Réussite :
-un accompagnement 3-en-1 pensé pour vous aider à décrocher des entretiens et réussir votre intégration professionnelle.
-
-👇 Découvrez le contenu du pack 👇
-            </p>
       </div>
     </section>;
 };
